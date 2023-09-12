3e10a1071fe0d4dcd2e3bd21c58fe3fd9308f77f
SaaFomenko <s.fomenko@list.ru>
2023-09-12 19:19:02 +0300

    Start project.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # multi-threads_data
 Example class Data for multi threads apps.
+
+Condition issue N 3 you see this [link](https://github.com/netology-code/map-homeworks/tree/main/02) 
---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <mutex>
+#include <iostream>
+#include <thread>
+
+
+namespace my
+{
+	class Data
+	{
+		private:
+			std::mutex m_;
+			int a_;
+
+		public:
+
+			Data(int a) : a_(a)
+			{}
+
+			~Data()
+			{}
+	}
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+
+	return 0;
+}
